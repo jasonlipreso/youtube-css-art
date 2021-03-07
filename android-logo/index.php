9fc5c4ce7d75f82a7ea07eb956ff3c4f4f638c0b
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2021-03-07 12:36:41 +0800

    1

---
+++ android-logo/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Android Logo | CSS Art</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+
+
+  <div class = "body">
+    <div class = "space">
+      <div class="arm-cont arm-left">
+        <div class = "kamot"></div>
+      </div>
+      <div class="body-cont">
+        <div class = "head-cont">
+          <div class = "sungay sungay-left"></div>
+          <div class = "sungay sungay-right"></div>
+          <div class = "head">
+            <div class = "eye eye-left"></div>
+            <div class = "eye eye-right"></div>
+          </div>
+        </div>
+        <div class = "lawas">
+          <div class = "leg leg-left"></div>
+          <div class = "leg leg-right"></div>
+        </div>
+      </div>
+      <div class="arm-cont arm-right">
+        <div class = "kamot"></div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+</body>
+</html>
---
+++ philippine-flag/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Philippine Flag</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class = "ph-flag">
+    <div class = "top"></div>
+    <div class = "bottom"></div>
+    <div class = "left">
+        <div class = "star-center"></div>
+        <div class = "rays-base" id = "ray-01">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-02">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-03">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-04">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-05">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-06">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-07">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "rays-base" id = "ray-08">
+          <div class = "ray-left"></div>
+          <div class = "ray-center"></div>
+          <div class = "ray-right"></div>
+        </div>
+        <div class = "star" id= "star-01"></div>
+        <div class = "star" id= "star-02"></div>
+        <div class = "star" id= "star-03"></div>
+    </div>
+  </div>
+</body>
+</html>
